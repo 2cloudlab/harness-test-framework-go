1a8d540943224007cffbd5b4cd468a4425682319
slz <zhengsl@cadg.cn>
2020-07-24 18:03:47 +0800

    implement auto run

--- auto-run.go
+++ auto-run.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+)
+
 func main() {
-	
+	svc := lambda.New(session.New())
+	params := EventParams{Iteration: 5, LambdaFunctionName: "worker-handler", CountInSingleInstance: 1}
+	payLoadInJson, _ := json.Marshal(params)
+	input := &lambda.InvokeInput{
+		FunctionName: aws.String("test-harness-framework"),
+		Payload:      payLoadInJson,
+	}
+	_, err := svc.Invoke(input)
+	if err != nil {
+		recordError(err)
+	}
 }
--- test-harness-framework.go
+++ test-harness-framework.go
@@ -8,75 +8,9 @@ import (
 	lambda_context "github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/lambdacontext"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/lambda"
 )
-
-func recordError(err error) {
-	if aerr, ok := err.(awserr.Error); ok {
-		switch aerr.Code() {
-		case lambda.ErrCodeServiceException:
-			fmt.Println(lambda.ErrCodeServiceException, aerr.Error())
-		case lambda.ErrCodeResourceNotFoundException:
-			fmt.Println(lambda.ErrCodeResourceNotFoundException, aerr.Error())
-		case lambda.ErrCodeInvalidRequestContentException:
-			fmt.Println(lambda.ErrCodeInvalidRequestContentException, aerr.Error())
-		case lambda.ErrCodeRequestTooLargeException:
-			fmt.Println(lambda.ErrCodeRequestTooLargeException, aerr.Error())
-		case lambda.ErrCodeUnsupportedMediaTypeException:
-			fmt.Println(lambda.ErrCodeUnsupportedMediaTypeException, aerr.Error())
-		case lambda.ErrCodeTooManyRequestsException:
-			fmt.Println(lambda.ErrCodeTooManyRequestsException, aerr.Error())
-		case lambda.ErrCodeInvalidParameterValueException:
-			fmt.Println(lambda.ErrCodeInvalidParameterValueException, aerr.Error())
-		case lambda.ErrCodeEC2UnexpectedException:
-			fmt.Println(lambda.ErrCodeEC2UnexpectedException, aerr.Error())
-		case lambda.ErrCodeSubnetIPAddressLimitReachedException:
-			fmt.Println(lambda.ErrCodeSubnetIPAddressLimitReachedException, aerr.Error())
-		case lambda.ErrCodeENILimitReachedException:
-			fmt.Println(lambda.ErrCodeENILimitReachedException, aerr.Error())
-		case lambda.ErrCodeEFSMountConnectivityException:
-			fmt.Println(lambda.ErrCodeEFSMountConnectivityException, aerr.Error())
-		case lambda.ErrCodeEFSMountFailureException:
-			fmt.Println(lambda.ErrCodeEFSMountFailureException, aerr.Error())
-		case lambda.ErrCodeEFSMountTimeoutException:
-			fmt.Println(lambda.ErrCodeEFSMountTimeoutException, aerr.Error())
-		case lambda.ErrCodeEFSIOException:
-			fmt.Println(lambda.ErrCodeEFSIOException, aerr.Error())
-		case lambda.ErrCodeEC2ThrottledException:
-			fmt.Println(lambda.ErrCodeEC2ThrottledException, aerr.Error())
-		case lambda.ErrCodeEC2AccessDeniedException:
-			fmt.Println(lambda.ErrCodeEC2AccessDeniedException, aerr.Error())
-		case lambda.ErrCodeInvalidSubnetIDException:
-			fmt.Println(lambda.ErrCodeInvalidSubnetIDException, aerr.Error())
-		case lambda.ErrCodeInvalidSecurityGroupIDException:
-			fmt.Println(lambda.ErrCodeInvalidSecurityGroupIDException, aerr.Error())
-		case lambda.ErrCodeInvalidZipFileException:
-			fmt.Println(lambda.ErrCodeInvalidZipFileException, aerr.Error())
-		case lambda.ErrCodeKMSDisabledException:
-			fmt.Println(lambda.ErrCodeKMSDisabledException, aerr.Error())
-		case lambda.ErrCodeKMSInvalidStateException:
-			fmt.Println(lambda.ErrCodeKMSInvalidStateException, aerr.Error())
-		case lambda.ErrCodeKMSAccessDeniedException:
-			fmt.Println(lambda.ErrCodeKMSAccessDeniedException, aerr.Error())
-		case lambda.ErrCodeKMSNotFoundException:
-			fmt.Println(lambda.ErrCodeKMSNotFoundException, aerr.Error())
-		case lambda.ErrCodeInvalidRuntimeException:
-			fmt.Println(lambda.ErrCodeInvalidRuntimeException, aerr.Error())
-		case lambda.ErrCodeResourceConflictException:
-			fmt.Println(lambda.ErrCodeResourceConflictException, aerr.Error())
-		case lambda.ErrCodeResourceNotReadyException:
-			fmt.Println(lambda.ErrCodeResourceNotReadyException, aerr.Error())
-		default:
-			fmt.Println(aerr.Error())
-		}
-	} else {
-		// Print the error, cast err to awserr.Error to get the Code and
-		// Message from an error.
-		fmt.Println(err.Error())
-	}
-}
 
 func LambdaHandler(ctx context.Context, params EventParams) (int, error) {
 	lc, _ := lambdacontext.FromContext(ctx)
