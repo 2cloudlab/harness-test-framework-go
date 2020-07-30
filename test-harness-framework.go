224fba55d6425e70c4ada76efe5cacd7f453faed
slz <zhengsl@cadg.cn>
2020-07-30 11:25:00 +0800

    add comment to test harness framework

--- test-harness-framework.go
+++ test-harness-framework.go
@@ -11,12 +11,19 @@ import (
 	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
+// Each request will route to this function.
+// So handle your request here.
+// Also make sure your lambda function is idempotent.
+// You should use fmt.Println() to log key info to Log Stream
 func LambdaHandler(ctx context.Context, params EventParams) (string, error) {
 	lc, _ := lambdacontext.FromContext(ctx)
 	params.RequestID = lc.AwsRequestID
 	payLoadInJson, _ := json.Marshal(params)
+	// Log payload to Log Stream for better analyze this function behavior
+	fmt.Println(payLoadInJson)
 	input := &lambda.InvokeInput{
-		FunctionName:   aws.String(params.LambdaFunctionName),
+		FunctionName: aws.String(params.LambdaFunctionName),
+		// Make the invoke async
 		InvocationType: aws.String("Event"),
 		Payload:        payLoadInJson,
 	}
@@ -26,12 +33,17 @@ func LambdaHandler(ctx context.Context, params EventParams) (string, error) {
 			recordError(err)
 		}
 	}
+	// Return requestID and success flag
 	return lc.AwsRequestID, nil
 }
 
+// Lambda will call main function only one time, when initialize.
+// So you should initialize global resources, such as external services client, in the main entry.
 func main() {
 	init_shared_resource()
-	fmt.Println("Before Start")
+	// The following command will block and wait for the request sending from clients.
+	// After idle for a duration, Lambda will automatically destroy the function instance.
 	lambda_context.Start(LambdaHandler)
-	fmt.Println("After Start")
+	// The following command will not log out to AWS Log Stream.
+	fmt.Println("End of this function instance")
 }
